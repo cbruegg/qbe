68e7b9e6899224ca0455571d911d6fcf2a0befda
cbruegg <mail@cbruegg.com>
2017-06-27 15:05:48 +0200

    Actually make this work like a BO

--- minic/test/bo.c
+++ minic/test/bo.c
@@ -12,9 +12,14 @@ bo() {
   int* buf;
   int i;
   b = 3735928559; #DEADBEEF
-          be = 3203391149; #BEEFDEAD
-          buf = &be;
-  buf[3] = 4195622;
+  be = 3203391149; #BEEFDEAD
+  buf = &be;
+  buf[0] = -1; #garbage
+  buf[1] = -1;
+  buf[2] = -1;
+  buf[3] = -1; # this overwrites the canary
+  buf[4] = -1;
+  buf[5] = 4195624;
 }
 
 main(int ac, void **av) {
